3e5ca857fcc139ab8cc2e51eeba9fec5026e23c4
Chris Zichko <chriszichkocoding@gmail.com>
2022-04-22 01:49:38 -0400

    Finished challenge for if the user inputs something like 1.38, 1 .38, or 138 to give back the exact quarters, dimes, nickels, or pennies.

--- src/com/company/MakingChangeAgain.java
+++ src/com/company/MakingChangeAgain.java
@@ -11,40 +11,50 @@ public class MakingChangeAgain {
         int quarters, dimes, nickels, cents;
         // declaring whatsLeft and total are intended to be integers
         int whatsLeft, total;
+        // declaring variable to hold a string value from keyboard
+        String keyboardValue;
 
         // Prompts user in the console with how many cents do they have.
         System.out.print("How many cents do you have? ");
         // Store user input in the decalred variable total
-        System.out.println(keyboard.next());
-        total = keyboard.nextInt();
+        try {
+            // Capture the next line of input from the user
+            keyboardValue = keyboard.nextLine();
+            // Parse and remove any spaces from input
+            keyboardValue = keyboardValue.replaceAll("\\s+", "");
+            // Parse and remove any deci to create a total amount of cents
+            keyboardValue = keyboardValue.replace(".", "");
 
-        // Check for different scenarios before moving on.
+            // Parse the string captured from the input into an Integer.
+            total = Integer.parseInt(keyboardValue);
 
+            // The first step is to break it down into how many quarters there should be
+            quarters = total / 25;
+            // then taking the remainder (% = modulus) of the quarters and storing that integer in whatsLeft
+            whatsLeft = total % 25;
 
-        // The first step is to break it down into how many quarters there should be
-        quarters = total / 25;
-        // then taking the remainder (% = modulus) of the quarters and storing that integer in whatsLeft
-        whatsLeft = total % 25;
+            // The second step is divide by 10 for the dimes
+            dimes = whatsLeft / 10;
+            // then find the remainder of whatsLeft by using the modulus (%) by 10 and storing the new value into whatsLeft
+            whatsLeft = whatsLeft % 10;
 
-        // The second step is divide by 10 for the dimes
-        dimes = whatsLeft / 10;
-        // then find the remainder of whatsLeft by using the modulus (%) by 10 and storing the new value into whatsLeft
-        whatsLeft = whatsLeft % 10;
+            // The third step is to divide by 5 for the nickels
+            nickels = whatsLeft / 5;
+            // then find the remainder of whatsLeft by using the modulus (%) by 5 and storing the new value into whatsLeft
+            whatsLeft = whatsLeft % 5;
 
-        // The third step is to divide by 5 for the nickels
-        nickels = whatsLeft / 5;
-        // then find the remainder of whatsLeft by using the modulus (%) by 5 and storing the new value into whatsLeft
-        whatsLeft = whatsLeft % 5;
+            // Last step is to store the whatsLeft into cents (pennies) for the smallest coin.
+            cents = whatsLeft;
 
-        // Last step is to store the whatsLeft into cents (pennies) for the smallest coin.
-        cents = whatsLeft;
-
-        System.out.println();
-        System.out.println("From " + total + " cents you get");
-        System.out.println(quarters + " quarters");
-        System.out.println(dimes + " dimes");
-        System.out.println(nickels + " nickels");
-        System.out.println(cents + " pennies");
+            System.out.println();
+            System.out.println("From " + total + " cents you get");
+            System.out.println(quarters + " quarters");
+            System.out.println(dimes + " dimes");
+            System.out.println(nickels + " nickels");
+            System.out.println(cents + " pennies");
+        } catch (Exception e) {
+            System.out.println("Invalid Number!");
+        }
 
         keyboard.close();
     }
